c63d8dae0e801bc4aaa1af00487119b3d25d9e94
Toqa Yassien <toqayassienn@yahoo.com>
2023-02-16 11:07:24 -0800

    4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,20 +1,16 @@
 #include <stdio.h>
 
 /**
- * main:
- * comment
- * for betty
+ * main -Entry point
+ * Description: Print "Programming is like building a multilingual puzzle
+ * Return: 0
  */
 int main(void)/*entry point*/
 {
-/*
- * another comment
- */
+
 	char Myster[100] = "\"Programming is like building a multilingual puzzle";
 
 	puts(Myster);
-/*
- * return:0 value comment
- */
+
 	return (0);
 }
