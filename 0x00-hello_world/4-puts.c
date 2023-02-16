89791dd0dd6ba8f3270f814035ee916b0c56d288
Toqa Yassien <toqayassienn@yahoo.com>
2023-02-16 10:58:50 -0800

    4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 
 /**
- * initialization comment
+ * main comment for betty
  */
-
 int main(void)/*entry point*/
 {
-
+/*
+ * another comment
+ */
 	char Myster[100] = "\"Programming is like building a multilingual puzzle";
 
 	puts(Myster);
