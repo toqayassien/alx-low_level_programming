57c64c73554c7884163438f1db6960fd94de4164
Toqa Yassien <toqayassienn@yahoo.com>
2023-02-16 11:52:46 -0800

    5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 
-/*
+/**
  * main -Entry point
  * Description: print "with proper grammar, but the outcome is a piece of art,"
  * Return:0
  */
-int main(void)
+int main(void)/*entry point*/
 {
 	char p[100] = "with proper grammar, but the outcome is a piece of art,\n";
 
