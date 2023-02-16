4c62769aa26b90f840a096f1c2a9513a6bd0f975
Toqa Yassien <toqayassienn@yahoo.com>
2023-02-16 12:10:58 -0800

    6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
- * Main -Entry point
+ * main -Entry point
  * Description: print sizes of data types
- * return:0
+ * Return: 0
  */
-int main(void)/*main:entry point*/
+int main(void)/*entry point*/
 {
 	int i;
 	char c;
@@ -12,11 +12,11 @@ int main(void)/*main:entry point*/
 	long int li;
 	long long int lli;
 	
-	printf("size of char:%zu byte\n",sizeof(c));
-	printf("size of int:%zu byte\n",sizeof(i));
-	printf("size of long int:%zu byte\n",sizeof(li));
-	printf("size of long long int:%zu byte\n",sizeof(lli));
-	printf("size of float:%zu byte\n",sizeof(f));
+	printf("size of char:%zu byte(s)\n", sizeof(c));
+	printf("size of int:%zu byte(s)\n", sizeof(i));
+	printf("size of long int:%zu byte(s)\n", sizeof(li));
+	printf("size of long long int:%zu byte(s)\n", sizeof(lli));
+	printf("size of float:%zu byte(s)\n", sizeof(f));
 
 	return (0);
 }
