002a388465112ee99085cb58538862cf80c0d103
Toqa Yassien <toqayassienn@yahoo.com>
2023-02-16 10:59:53 -0800

    4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -11,6 +11,8 @@ int main(void)/*entry point*/
 	char Myster[100] = "\"Programming is like building a multilingual puzzle";
 
 	puts(Myster);
-
-	return (0); /*return value for main*/
+/*
+ * return value comment
+ */
+	return (0);
 }
