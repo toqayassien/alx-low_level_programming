f0d8dac69c986c44c53942b9627be2fa0cf8eba6
Toqa Yassien <toqayassienn@yahoo.com>
2023-02-20 05:41:29 -0800

    0

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main -Entry point
+ * Description:last digit
+ * Return:0
+ */
+int main(void)/*main:description for betty */
+{
+	int n;
+	int l=n%10;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	
+	if (l > 5)
+		printf("Last digit of %d is, %d and is greater than 5\n", n, l);
+	else if (l < 6)
+		printf("Last digit of %d is, %d and is less than 6 and not 0\n", n, l);
+	else if (l = 0)
+		printf("Last digit of %d is, %d and is 0\n", n, l);
+	return (0);
+}	
