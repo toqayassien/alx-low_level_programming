ca623bf845077a445f2b8a0af18655f5c9838d3b
Toqa Yassien <toqayassienn@yahoo.com>
2023-02-20 04:42:26 -0800

    0

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -6,18 +6,19 @@
  * Description:negative or positive
  * Return:0
  */
-int main (void)
+int main (void) /*main description for betty*/
 {
 	int n;
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-		if(n>0)
-		{ printf("%d is positive\n",n);
-		} else if (n<0){
-	printf ("%d is negative\n",n);
-		} else {
-	printf ("%d is zero\n",n);
-		}
+
+		if( n > 0 )
+		printf("%d is positive\n", n);
+		 else if( n < 0)
+	printf ("%d is negative\n", n);
+		 else 
+	printf ("%d is zero\n", n);
+		
 	return(0);
 }
 
