ce25d80721378c5407251828ae4a50ad929341a9
Toqa Yassien <toqayassienn@yahoo.com>
2023-02-20 06:29:51 -0800

    3

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,1 +1,18 @@
 #include <stdio.h>
+/**
+ * main -Entry point
+ * Description:upper and lower alphabet
+ * Return:0
+ */
+int main(void)/*main: description for betty*/
+{
+	char ch = 'a';
+	char CH = 'A';
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+		putchar(ch);
+	for (CH = 'A'; ch <= 'Z'; ch++)
+		putchar(CH);
+	putchar('\n');
+	return (0);
+}
