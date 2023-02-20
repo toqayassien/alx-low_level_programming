1716acc215aa3250f4b4877d1a5cfb5b3e9a5541
Toqa Yassien <toqayassienn@yahoo.com>
2023-02-20 05:56:09 -0800

    1

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -13,13 +13,13 @@ int main(void)/*main:description for betty */
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	
+
 	l = n % 10;
 	if (l > 5)
 		printf("Last digit of %d is %d and is greater than 5\n", n, l);
-	else if (l < 6)
+	else if (l < 6,l != 0)
 		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, l);
-	else
+	else if (l = 0)
 		printf("Last digit of %d is %d and is 0\n", n, l);
 	return (0);
-}	
+}
