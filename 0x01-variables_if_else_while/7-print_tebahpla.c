2ed9e911480437492f19e265486efa2abe5ab700
Toqa Yassien <toqayassienn@yahoo.com>
2023-02-20 08:05:52 -0800

    7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main -Entry point
+ * Description:reverse alphabet
+ * Return:0
+ */
+int main(void)/*main:description for betty*/
+{
+	char ch;
+
+	for( ch = 'z'; ch >= 'a'; ch--)
+		putchar(ch);
+	putchar('\n');
+	return (0);
+}
