2dddd867b71c61c2e8e945380ca5a462287b8caa
Toqa Yassien <toqayassienn@yahoo.com>
2023-02-20 09:27:33 -0800

    11

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+/**
+ * main -Entry point
+ * Description:combo
+ * Return:0
+ */
+int main(void)/*main: for betty*/
+{
+	int x;
+	int y;
+	int z;
+
+	for (x = 0; x < 8; x++)
+	{
+		for (y = 0; y < 9; y++)
+		{
+			for (z = 0; z < 10; z++)
+			{
+				putchar ((x % 10) + '0');
+				putchar ((y % 10) + '0');
+				putchar ((z % 10) + '0');
+				
+				if (x == 7 && y == 8 && z == 9)
+					continue;
+				putchar (',');
+				putchar (' ');
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
