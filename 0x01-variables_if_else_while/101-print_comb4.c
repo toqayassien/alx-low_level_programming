04022d1d049894648a62646b76fd12d6e495077e
Toqa Yassien <toqayassienn@yahoo.com>
2023-02-20 09:30:54 -0800

    11

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -12,14 +12,14 @@ int main(void)/*main: for betty*/
 
 	for (x = 0; x < 8; x++)
 	{
-		for (y = 0; y < 9; y++)
+		for (y = x + 1; y < 9; y++)
 		{
-			for (z = 0; z < 10; z++)
+			for (z = y + 1; z < 10; z++)
 			{
 				putchar ((x % 10) + '0');
 				putchar ((y % 10) + '0');
 				putchar ((z % 10) + '0');
-				
+
 				if (x == 7 && y == 8 && z == 9)
 					continue;
 				putchar (',');
