1f5d41ea107a839975a4c56dc4b6b74f542d0292
Toqa Yassien <toqayassienn@yahoo.com>
2023-02-20 07:10:14 -0800

    4

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -8,7 +8,7 @@ int main(void)/*main: for betty*/
 {
 	char ch = 'a';
 
-	for (ch != 'a' && ch != 'q'; ch <= 'z'; ch++)
+	for (ch = 'a' && ch != 'e' && ch != 'q'; ch <= 'z'; ch++)
 		putchar(ch);
 	putchar('\n');
 	return (0);
