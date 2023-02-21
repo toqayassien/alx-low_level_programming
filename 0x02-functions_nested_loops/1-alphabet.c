53109730c3f14f1707a6d2f84cdada6499a00403
Toqa Yassien <toqayassienn@yahoo.com>
2023-02-21 12:08:09 -0800

    1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main -entry point
+ * desc: print alphabet
+ * Return:0
+ */
+int main(void)
+{
+void print_alphabet(void)
+{
+	char a;
+	
+	for (a = 'a'; a <= 'z'; a++)
+		putchar(a);
+	putchar('\n');
+}
+	return (0);
+	}
