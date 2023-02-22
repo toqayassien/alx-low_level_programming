069dbb92beb4291f3e1288d621fe3ea83033f8e1
Toqa Yassien <toqayassienn@yahoo.com>
2023-02-22 14:18:11 -0800

    11

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -7,9 +7,16 @@
  */
 void print_to_98(int n)
 {
-	if (n <=98)
-		{
-			for (n = 0; n <= 98; n++)
-				printf("%d, ", n);
-		}
+	if (n > 98)
+	{
+		while (n > 98)
+			printf("%d, ", n--);
+		printf("%d\n", n);
+	}
+	else
+	{ 
+		while ( n < 98)
+			printf("%d, ", n++);
+		printf("%d\n", n);
+	}
 }
