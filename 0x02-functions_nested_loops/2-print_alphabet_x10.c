a898641a270adc62168a784fe1ecd3ae27243759
Toqa Yassien <toqayassienn@yahoo.com>
2023-02-22 09:42:22 -0800

    2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,19 +1,17 @@
 #include "main.h"
 /**
- *
- *
+ *print_alphabet_x10 -entry point
+ *desc: print alphabet x10
  */
-void print_alphabet_x10(void)
+void print_alphabet_x10(void)/*print_alphabet_x10: for betty*/
 {
-	int x;
+	int x=0;
 	char c;
 	
 	while (x <= 9);
 	{
 		for (c = 'a'; c <= 'z'; c++)
-		{
 			_putchar(c);
 		_putchar('\n');
-	}
 	}
 }
