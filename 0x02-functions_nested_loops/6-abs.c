0d4b335469ce171b8e83551b7db5a049ad201a16
Toqa Yassien <toqayassienn@yahoo.com>
2023-02-22 10:53:02 -0800

    6

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -3,13 +3,14 @@
  * _abs -entry point
  * desc: absolute value
  * Return:0
+ * @x:number
  */
 int _abs(int x)/*_abs: for betty*/
 {
 
 	if (x >= 0)
 		return (x);
-	else 
+	else
 		return (-x);
 	return (0);
 }
