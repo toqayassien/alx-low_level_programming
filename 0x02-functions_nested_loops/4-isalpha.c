ad152ccb58cc4de4469fa1ec239678f360d7610b
Toqa Yassien <toqayassienn@yahoo.com>
2023-02-22 10:26:45 -0800

    4

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -2,13 +2,14 @@
 /**
  * _isalpha -entry point
  * desc:lower or upper
+ * @c: letter
  *Return:0
  */
 int _isalpha(int c)/*_isalpha:for betty*/
 {
 	if ((c = 'a' && c <= 'z') ||
 		(c = 'A' && c <= 'Z'))
-			return (1);
+		return (1);
 	else
 		return (0);
 	return (0);
