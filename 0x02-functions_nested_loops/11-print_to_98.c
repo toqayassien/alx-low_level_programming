3262ecab4a6a4add01487180b33f5788eca7c508
Toqa Yassien <toqayassienn@yahoo.com>
2023-02-22 14:01:10 -0800

    11

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ *print_to_98 -entry point
+ *description: all natural numbers
+ *Return:0
+ */
+void print_to_98(int n)
+{
+	if (n <=98)
+		{
+			for (n = 0; n <= 98; n++)
+				_printf("%d, ", n++);
+		}
+}
