61d178377eafbbbc06c06c09709d7f8bcce995b5
Toqa Yassien <toqayassienn@yahoo.com>
2023-02-22 10:00:45 -0800

    3

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ *_islower -entry point
+ *desc: lower case
+ */
+int _islower(int c)/*_islower: for betty*/
+{
+	if (c >= 'a' && c <= 'z')
+		return(1);
+	else
+		return(0);
+}
