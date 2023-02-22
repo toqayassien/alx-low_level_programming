e435a3517348739465aaaa19f37368984d77a3e6
Toqa Yassien <toqayassienn@yahoo.com>
2023-02-22 11:13:02 -0800

    7

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -3,12 +3,12 @@
  * print_last_digit -entry point
  * desc: last digit
  * Return:0
- * @x
+ * @x:num
  */
 int print_last_digit(int x)/*print_last_digit: for betty*/
 {
 	int l = x % 10;
-	
+
 	if (l < 0)
 		l *= -1;
 
