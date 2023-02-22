8d271bed1ffadae259987adb9999562310d3dec0
Toqa Yassien <toqayassienn@yahoo.com>
2023-02-22 05:38:07 -0800

    1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -6,10 +6,13 @@
  */
 int main(void)/*main: for betty*/
 {
+	void print_alphabet(void)
+	{
 	char ch;
 
 	for (ch = 'a'; ch <= 'z'; ch++)
 		_putchar(ch);
 	_putchar('\n');
-	return (0);
+	return;
+	}
 }
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -2,7 +2,7 @@ int _putchar(char c)
 {
 	return (write(1, &c, 1));
 }
-void print_alphabet(void);
+void print_alphabet(void)
 {
 	char ch;
 
