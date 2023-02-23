f2f7b69b16e2350c5e785d1ec98b43fdf7e8a909
Toqa Yassien <toqayassienn@yahoo.com>
2023-02-23 04:29:02 -0800

    0

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ *main -tests function that prints if integer is positive or negative
+ *Return:0
+ */
+int main(void)/*main: for betty*/
+{
+	int i;
+
+	if ( i > 0)
+	positive_or_negative(i);
+	return (0);
+}
