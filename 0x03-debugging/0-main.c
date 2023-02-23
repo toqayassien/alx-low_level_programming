ecb65e3c00d5921f38878d274ac5b68d93bd795d
Toqa Yassien <toqayassienn@yahoo.com>
2023-02-23 04:42:06 -0800

    0

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -3,11 +3,9 @@
  *main -tests function that prints if integer is positive or negative
  *Return:0
  */
-int main(void)/*main: for betty*/
+void positive_or_negative(int i)/*main: for betty*/
 {
-	int i;
-
-	if ( i > 0)
+	
+	for (i = 0; i <= 98; i++)
 	printf("%d is positive", i);
-	return (0);
 }
