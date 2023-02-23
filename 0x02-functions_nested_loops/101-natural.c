b42ebb5f0d49cd2cea13e6fada36daf48b6f2a80
Toqa Yassien <toqayassienn@yahoo.com>
2023-02-22 16:06:18 -0800

    11

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -12,9 +12,9 @@ int main(void)/*naturals: for betty*/
 
 	for (i = 0; i < 1024; i++)
 	{
-			if ((i % 3) == 0 || (i % 5) == 0)
-				sum += i;
+		if ((i % 3) == 0 || (i % 5) == 0)
+			sum += i;
 	}
-		printf("%d\n", sum);
+	printf("%d\n", sum);
 	return (0);
 }
