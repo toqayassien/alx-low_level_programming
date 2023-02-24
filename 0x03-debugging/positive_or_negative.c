5aa584bf37bc87ed8f7f3897e3ecf4470693433d
Toqa Yassien <toqayassienn@yahoo.com>
2023-02-24 15:22:26 -0800

    0

--- 0x03-debugging/positive_or_negative.c
+++ 0x03-debugging/positive_or_negative.c
@@ -3,15 +3,15 @@
 void positive_or_negative(int i)/*print_sign: for betty*/
 {
 	if (i > 0)
-	{	_putchar('+');
+	{	putchar('+');
 		return (1);
 	}
 	else if (i < 0)
-	{	_putchar('-');
+	{	putchar('-');
 		return (-1);
 	}
 	else
-	{	_putchar('0');
+	{	putchar('0');
 		return (0);
 	}
 	return (0);
