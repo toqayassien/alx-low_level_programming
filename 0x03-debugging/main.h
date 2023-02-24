d2f4b3956e6c3fe2dbb1e17f83c1097b26851244
Toqa Yassien <toqayassienn@yahoo.com>
2023-02-24 15:10:07 -0800

    0

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -3,6 +3,21 @@
 
 #include <stdio.h>
 
-void positive_or_negative(int i);
+int positive_or_negative(int i)
+{
+	int n;
 
+if (n > 0)
+	{	putchar('+');
+		return (1);
+	}
+	else if (n < 0)
+	{	putchar('-');
+		return (-1);
+	}
+	else
+	{	putchar('0');
+		return (0);
+	}
+}
 #endif /* MAIN_H */
