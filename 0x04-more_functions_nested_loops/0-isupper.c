0ccbd4cd072d5d5d2802f13f8cb28433372f1b99
Toqa Yassien <toqayassienn@yahoo.com>
2023-02-24 14:54:48 -0800

    0

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ *_isupper -main entry point
+ *Return: 0
+ */
+int _isupper(int c)
+{
+	char ch;
+
+	if (ch = 'A')
+	{
+	for (ch = 'A'; ch <= 'Z'; ch++)
+		return (1);
+	}
+	else 
+		return (0);
+	
+	return (0);
+}
