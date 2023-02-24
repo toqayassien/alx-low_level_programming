201b3a0c313e844d9746b30e9bb1b7d81f98b893
Toqa Yassien <toqayassienn@yahoo.com>
2023-02-24 15:40:28 -0800

    1

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -12,20 +12,17 @@ int largest_number(int a, int b, int c)
 {
    	int largest;
 
-    if (a > b && b > c)
+    if (a > b && a > c)
     {
 	    largest = a;
-        printf("%d is the largest number\n", largest);
     }
-    else if (b > a && a > c)
+    else if (b > a && b > c)
     {
-	    largest b:
-        printf("%d is the largest number\n", largest);
+	    largest = b;
     }
     else
     {
-	    largest c;
-        printf("%d is the largest number\n", largest);
+	    largest = c;
     }
-    return (0);
+    return (largest);
 }
