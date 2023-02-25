20a65214eeebed1a127ae623f67da66005e8f7d3
Toqa Yassien <toqayassienn@yahoo.com>
2023-02-25 09:03:22 -0800

    0

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,10 +1,11 @@
 #include "main.h"
 /**
  *_isupper -main entry point
- *Return: 0
+ *Return:0
  *desc: uppercase
+ *@c: letter
  */
-int _isupper(int c)
+int _isupper(int c)/*_isupper: for betty*/
 {
 	if (c >= 'A' && c <= 'Z')
 		return (1);
