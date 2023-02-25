14470d615e7bcbc70479a95d8beba22869087342
Toqa Yassien <toqayassienn@yahoo.com>
2023-02-25 09:13:11 -0800

    1

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -3,10 +3,13 @@
  *_isdigit -main entry
  *@c: digit
  *Desc: digit or not
+ *Return:0
  */
 int _isdigit(int c)/*_isdigit: for betty*/
 {
 	if (c >= 0 && c <= 9)
 		return (1);
+	else 
+		return (0);
 	return (0);
 }
