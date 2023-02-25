ba1041a36588fd9d395638d72e790d857f10dbdc
Toqa Yassien <toqayassienn@yahoo.com>
2023-02-25 09:46:01 -0800

    3

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ *print_numbers -entry point
+ *Desc: print numbers
+ */
+void print_numbers(void)
+{
+	int x;
+
+	for (x = 0; x <= 9; x++)
+		_putchar(x);
+	_putchar('\n');
+}
