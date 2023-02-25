549bc9f280269101df08240a606fb3f4349b3bfb
Toqa Yassien <toqayassienn@yahoo.com>
2023-02-25 10:25:42 -0800

    4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ *print_most_numbers -entry point
+ *desc: not 2 and 4
+ */
+void print_most_numbers(void)/*print_most_numbers*/
+{
+	int x;
+
+	for (x = '0'; x <= '9'; x++)
+	{
+		if (x == 2 || x == 4)
+			break;
+		_putchar(x);
+	}
+	_putchar('\n');
+}
