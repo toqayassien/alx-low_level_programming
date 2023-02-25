ff3e4286a2a0a11ff970b6e5d43aee8fbfd58094
Toqa Yassien <toqayassienn@yahoo.com>
2023-02-25 11:00:11 -0800

    5

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ *Desc: 10 times
+ *more_numbers -entry point
+ */
+void more_numbers(void)/*more_numbers: for betty*/
+{
+	int x;
+	int count;
+
+	for (count = 0; count <= 9; count++)
+	{
+		for(x = 0; x <= 14; x++)
+		{
+			if (x > 9)
+				_putchar (x % 10);
+		_putchar(x);
+		}
+	}
+	_putchar('\n');
+}
