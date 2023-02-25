cf59e9bb3a7a97cf4104cd9914a9496a2350f6fa
Toqa Yassien <toqayassienn@yahoo.com>
2023-02-25 13:20:56 -0800

    9

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ *main -main entry
+ *Desc: FizzBuzz
+ */
+int main(void)/*main: for betty*/
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+		printf("%d\n", i);
+	{
+		if ((i % 3) == 0)
+			printf("\nFizz\n");
+		else if ((i % 5) == 0)
+			printf("\nBuzz\n");
+		else if ((i % 5) == 0 && (i % 3) == 0)
+			printf("\nFizzBuzz\n");
+	}
+}
