7d03909c9c463b9bb2d9044b2882a700b0757af1
Toqa Yassien <toqayassienn@yahoo.com>
2023-02-25 13:07:03 -0800

    8

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ *print_square -main entry
+ *Desc: print square
+ *@size: of square
+ */
+void print_square(int size)/*print_square: for betty*/
+{
+	int hgt, wid;
+
+	if (size > 0)
+	{
+		for (hgt = 0; hgt < size; hgt++)
+		{
+			for (wid = 0; wid < size; wid++)
+				_putchar('#');
+			if (hgt == size -1)
+				continue;
+			_putchar('\n');
+		}
+	}
+	_putchar('\n');
+}
