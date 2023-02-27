1b2d4bbb2c0455bc4d3f1a17d18be19a63ffae83
Toqa Yassien <toqayassienn@yahoo.com>
2023-02-27 05:36:16 -0800

    0

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ *reset_to_98 -main entry
+ *return:0
+ @n: pointer
+ */
+void reset_to_98(int *n)/*reset_to_98 : for betty*/
+{
+	int a;
+	int *p;
+
+	p = &a;
+	*p = 98;
+
+	_putchar(a);
+}
