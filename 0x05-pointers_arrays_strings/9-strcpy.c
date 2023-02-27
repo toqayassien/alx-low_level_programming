c808a616818126a3f0dab8c2cc3ec84012d7169a
Toqa Yassien <toqayassienn@yahoo.com>
2023-02-27 08:59:53 -0800

    9

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -1,16 +1,17 @@
 #include "main.h"
 /**
  **_strcpy -main
- *@dest:
- *@src:
+ *@dest: str1
+ *@src: str2
+ *return:dest
  */
 char *_strcpy(char *dest, char *src)
 {
 	int index = 0;
-	
+
 	while (src[index])
 	{
-		dest[index] = src[index];
+		dest[index + 1] = src[index + 1];
 		index++;
 	}
 	return (dest);
