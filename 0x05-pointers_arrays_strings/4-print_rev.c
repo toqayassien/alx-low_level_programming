fd3e74de3c361279379ac28aebd69f292e58c57e
Toqa Yassien <toqayassienn@yahoo.com>
2023-02-27 08:16:30 -0800

    4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ *print_rev -main
+ *@s: reverse
+ */
+void print_rev(char *s)/*print_rev : for betty*/
+{
+	int len;
+	int index;
+
+	len = 0;
+
+	while (s[index++])
+		len++;
+
+	for (index = len -1; index >= 0; index--)
+		_putchar(s[index]);
+
+	_putchar('\n');
+}
