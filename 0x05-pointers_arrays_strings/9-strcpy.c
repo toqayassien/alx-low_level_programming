b6018944124f836d2339d55967d9c77a74e1a995
Toqa Yassien <toqayassienn@yahoo.com>
2023-02-27 08:54:57 -0800

    9

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ **_strcpy -main
+ *@dest:
+ *@src:
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int index = 0;
+	
+	while (src[index])
+	{
+		dest[index] = src[index];
+		index++;
+	}
+	return (dest);
+}
