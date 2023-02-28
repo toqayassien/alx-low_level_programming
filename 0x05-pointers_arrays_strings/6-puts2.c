cd32a9aab8fe02d7690ceedb74f03082ffe3b312
Toqa Yassien <toqayassienn@yahoo.com>
2023-02-28 09:25:08 -0800

    6

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ *puts2 -main entry
+ *@str: string
+ */
+void puts2(char *str)/*puts2 : for betty*/
+{
+	int len = 0;
+	int index = 0;
+
+	while (str[index++])
+		len++;
+
+	for (index = len - 1; index >= len - 2; index++)
+		_putchar(str[index]);
+}
