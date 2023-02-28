5fa75a2b9409c2d65fbfbea076f28a32932a1f42
Toqa Yassien <toqayassienn@yahoo.com>
2023-02-28 10:31:16 -0800

    9

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -7,12 +7,5 @@
  */
 char *_strcpy(char *dest, char *src)
 {
-	int index = 0;
-
-	while (src[index])
-	{
-		dest[index] = src[index];
-		index++;
-	}
-	return (dest);
+	*dest = *src;
 }
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -8,3 +8,4 @@ char *_strcpy(char *dest, char *src);
 void puts2(char *str);
 void puts_half(char *str);
 void print_array(int *a, int n);
+char *_strcpy(char *dest, char *src);
