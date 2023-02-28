7a8be70f810ee516aeb537f9981e9118bbd09ad3
Toqa Yassien <toqayassienn@yahoo.com>
2023-02-28 09:48:53 -0800

    7

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ *puts_half -main entry
+ *@str : string to split
+ */
+void puts_half(char *str)/*puts_half: for betty*/
+{
+	int len = 0;
+	int index = 0;
+	int n;
+
+	while (str[index++])
+		len++;
+
+	if (len % 2 == 0)
+		n = len / 2;
+
+	else
+		n = (len - 1) / 2;
+	for (index = n; index < len; index++)
+		_putchar(str[index]);
+	_putchar('\n');
+}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -6,3 +6,4 @@ void print_rev(char *s);
 void rev_string(char *s);
 char *_strcpy(char *dest, char *src);
 void puts2(char *str);
+void puts_half(char *str);
