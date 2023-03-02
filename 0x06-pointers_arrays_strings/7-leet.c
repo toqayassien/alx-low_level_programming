b91bf9e8bef2fdaead3ac97b00dfea0074dbc3c9
Toqa Yassien <toqayassienn@yahoo.com>
2023-03-02 12:10:07 -0800

    7

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ *leet -main entry
+ *@str: string to encode
+ *Return: str
+ */
+char *leet(char *str)
+{
+	char a[] = {'a', 'e', 'o', 't', 'i'};
+	char b[] = {4, 3, 0, 7, 1};
+	int i = 0;
+
+	while (*str)
+	{
+		for (i = 0; i < 5; i++)
+		{
+			if (*str == a[i] || *str == a[i] - 32)
+				*str = b[i] + '0';
+		}
+		str++;
+	}
+	return (str);
+}
