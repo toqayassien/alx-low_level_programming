1b60f3c640b48998c6c1e3c414429af255126026
Toqa Yassien <toqayassienn@yahoo.com>
2023-03-02 11:09:45 -0800

    6

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -12,7 +12,7 @@ char *cap_string(char *str)
 	{
 		while (str[i] >= 'a' && str[i] <= 'z')
 			i++;
-		
+		{
 		if (str[i - 1] == ' ' ||
 		    str[i - 1] == '\t' ||
 		    str[i - 1] == '\n' ||
@@ -28,6 +28,7 @@ char *cap_string(char *str)
 		    str[i - 1] == '}' ||
 		    i == 0)
 			str[i] -= 32;
+		}
 		i++;
 	}
 	return (str);
