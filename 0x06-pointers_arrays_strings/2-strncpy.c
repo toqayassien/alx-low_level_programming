41e23023654b0e2b5cdaf69bb985f52d37edb52d
Toqa Yassien <toqayassienn@yahoo.com>
2023-03-02 06:11:40 -0800

    2

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ *_strncpy -main point
+ *@dest: destination
+ *@src : source 
+ *@n: number of char
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int index = 0;
+	int len = 0;
+
+	while (dest[index++])
+		len++;
+
+	for (index = 0; src[index] && index < n; index++)
+		dest[index] = src[index];
+
+	for (index = len; index < n; index++)
+		dest[index] = '\0';
+	return (dest);
+}
