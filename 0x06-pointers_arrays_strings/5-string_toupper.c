6376f33f12ea97809f9a63ea03dddccad5cecb31
Toqa Yassien <toqayassienn@yahoo.com>
2023-03-02 10:18:00 -0800

    5

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -2,7 +2,7 @@
 /**
  *string_toupper -main entry
  *Return : string
- *@str : str
+ *@str : string
  */
 char *string_toupper(char *str)
 {
@@ -11,7 +11,7 @@ char *string_toupper(char *str)
 	while (str[i])
 	{
 		if (str[i] >= 'a' && str[i] <= 'z')
-			str[i] -=32;
+			str[i] -= 32;
 		i++;
 	}
 	return (str);
