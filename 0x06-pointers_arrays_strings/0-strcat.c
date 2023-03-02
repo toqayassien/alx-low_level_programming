17705e9f9367961642f6722ecd427f619c8e8fff
Toqa Yassien <toqayassienn@yahoo.com>
2023-03-02 05:14:55 -0800

    0

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ **_strcat -main entry
+ *@*dest: destination
+ *@*src: source
+ *Return: dest
+ */
+char *_strcat(char *dest,char *src)
+{
+	int index = 0;
+	int dest_len = 0;
+
+	while (dest[index++])
+		dest_len++;
+
+	for (index = 0; src[index]; index++)
+		dest[dest_len++] = src [index];
+	return (dest);
+}
