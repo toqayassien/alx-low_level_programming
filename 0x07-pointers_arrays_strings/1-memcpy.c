8ffc52071954a2c9a20e8a503e7c7ea3beab9c13
Toqa Yassien <toqayassienn@yahoo.com>
2023-03-07 11:33:20 -0800

    1

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ *_memcpy -main
+ *@src : source
+ *@dest: destination
+ *@n : bytes
+ *Return: pointer to dest
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	char *fir = dest;
+	char *sec = src;
+	unsigned int m;
+
+	for (m = 0; m < n; m++)
+		fir[m] = *sec;
+	return (fir);
+}
