8d1c2059b6e33970151718b7555db89c97ccc889
Toqa Yassien <toqayassienn@yahoo.com>
2023-03-07 11:59:57 -0800

    3

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ *_strspn -main
+ *@s : string
+ *@accept : idk
+ *Return: number
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	int i;
+	unsigned int byte = 0;
+
+	while (*s)
+	{
+		for (i = 0; accept [i]; i++)
+		{
+			if (*s == accept[i])
+			{
+				byte++;
+				break;
+			}
+			else if (accept[i + 1] == '\0')
+				return (byte);
+		}
+	}
+	return (byte);
+}
