6815b98b38e4c6a06a05ecd1ef206083a100a729
Toqa Yassien <toqayassienn@yahoo.com>
2023-03-08 01:06:38 -0800

    0

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,10 +1,13 @@
 #include "main.h"
 /**
  *_puts_recursion -main entry
- *@s :string 
+ *@s :string
  */
 void _puts_recursion(char *s)
 {
-	_putchar(*s);
+	while (*s)
+	{
+		_putchar('s');
+	}
 	_putchar('\n');
 }
