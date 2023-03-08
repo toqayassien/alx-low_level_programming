8f2a58b960ddad921f6c5b61c16fee2d1149b610
Toqa Yassien <toqayassienn@yahoo.com>
2023-03-08 01:51:49 -0800

    3

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -2,15 +2,15 @@
 /**
  *factorial -main entry
  *@n: num
- *Return : int
+ *Return: int
  */
 int factorial(int n)
 {
 	if (n < 0)
-		return -1;
+		return (-1);
 
 	else if (n == 0 || n == 1)
-		return 1;
+		return (1);
 
 	n *= factorial(n - 1);
 
