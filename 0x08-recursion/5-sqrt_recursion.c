d184551f3c4ff73577e3fb07f9bc473d473e570a
Toqa Yassien <toqayassienn@yahoo.com>
2023-03-08 02:12:51 -0800

    5

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ *_sqrt_recursion -main
+ *@n: number
+ *Return: result
+ */
+int _sqrt_recursion(int n)
+{
+	int x = n;
+
+	if (n == 0)
+		return (-1);
+
+	x *= _sqrt_recursion(n - 1);
+	return (x);
+}
