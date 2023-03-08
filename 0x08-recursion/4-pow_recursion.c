c6b2f55bae0303ee62647810459f6f1c2a9b43f6
Toqa Yassien <toqayassienn@yahoo.com>
2023-03-08 01:55:35 -0800

    4

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ *_pow_recursion -main entry
+ *@x: int a
+ *@y: int power
+ *Return: result
+ */
+int _pow_recursion(int x, int y)
+{
+	int n;
+
+	if (y < 0)
+		return (-1);
+
+	n = _pow_recursion(x , y);
+	return (n);
+}
