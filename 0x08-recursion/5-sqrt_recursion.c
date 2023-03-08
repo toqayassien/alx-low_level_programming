dd8398b0cde3cf6dccba521da858c95c72fe4aa0
Toqa Yassien <toqayassienn@yahoo.com>
2023-03-08 11:38:07 -0800

    5

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -8,14 +8,13 @@ int _sqrt_recursion(int n)
 {
 	int x;
 
-	x * x = n;
-
 	if (n <= 0)
 		return (-1);
 
 	else if (n == 1)
 		return (1);
 
-	x = _sqrt_recursion(n);
+	else if (n > 1)
+		x * x == n;
 	return (x);
 }
