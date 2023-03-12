b3d04e5c938820ad6dd63b0dd6861780ee42365c
Toqa Yassien <toqayassienn@yahoo.com>
2023-03-12 07:02:13 -0700

    1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ *main -main entry
+ *@argc: arg count
+ *@argv: arg vec
+ *Return: 0
+ */
+int main(int argc, char __attribute__((__unused__)) *argv[])
+{
+	printf("%d\n", argc);
+	return (0);
+}
+
