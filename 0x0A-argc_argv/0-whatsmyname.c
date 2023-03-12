cd752ad1f804566ddd814550f3e33260b93a2ad9
Toqa Yassien <toqayassienn@yahoo.com>
2023-03-12 06:50:31 -0700

    0

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,5 +1,6 @@
 #include "main.h"
 #include <stdio.h>
+#include <stdlib.h>
 /**
  *main -main entry
  *@argc: arg count
@@ -11,3 +12,4 @@ int main(int argc, char *argv[])
 	printf("%s\n", argv[0]);
 	return (0);
 }
+
